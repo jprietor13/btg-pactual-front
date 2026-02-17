56af5b065bf75b58fc0ba14273e0d8f7ff15cd02
Juan Prieto <jp1739@gmail.com>
2026-02-17 16:49:25 -0500

    feat: remove token when response.status is 401

--- src/core/api/httpClient.ts
+++ src/core/api/httpClient.ts
@@ -17,3 +17,16 @@ httpClient.interceptors.request.use((config) => {
 
   return config;
 });
+
+// respuesta del interceptor
+httpClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("access_token");
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
